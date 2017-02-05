c5fa3fa24fe6e4b485c8d031e45208dc51f3d020
burakflatiron <burakflatiron@Tech-Book.home>
2017-02-05 15:22:19 -0500

    Finished matt bool op con switch Lab

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -11,13 +11,80 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    func averageIsAbove75(a argOne:Double, b argTwo:Double, c argThree:Double) -> Bool
+    {
+        let calc = (argOne + argTwo + argThree)
+        let divcalc = calc / 3
+        
+        if (divcalc>75)
+        {
+            return true
+        }
+        else
+        {
+        return false
+        }
+    }
     
+    func passwordCombo(username:String, password:Int) -> String
+    {
+        if (username=="Jerry" ||  username=="Elaine" || username=="Michael")
+        {
+            
+           
+            
+            if (password % 3 == 0)
+            {
+                return "Welcome!"
+            }
+            else
+            {
+                return "Access Denied"
+            }
+            
+        }
+        else
+        {
+            return "Access Denied"
+        }
+        
+        
+    }
     
-    
-    // Implement your functions here
-    
-    
-    
+    func describe(emoji:String) -> String{
+        
+        if (emoji=="💋")
+        {
+            return "Kiss"
+        }
+        else if (emoji=="🐈")
+        {
+                    return "Cat"
+        }
+        else if (emoji=="🐢")
+        {
+            return "Turtle"
+        }
+        else if (emoji=="🍕")
+        {
+            return "Pizza"
+        }
+        else if (emoji=="👻")
+        {
+            return "Ghost"
+        }
+        else
+        {
+            return "Unknown"
+        }
+        
+
+    }
+   
     
 
 }
+extension Double {
+    var isInteger: Bool {return rint(self) == self}
+}
+
